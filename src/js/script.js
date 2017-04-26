32c475f8ce5abdb66e47590e8988ff078005054f
KarryNefedieva <karinefedieva@gmail.com>
2017-04-27 00:07:53 +0300

    1.9

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,41 @@
-$(document).ready(function(){
-// $(".main_nav").hide(1500);
-
-
-    $(".main_screen__title").click(function(){
-    $(this).css("background-color","red")
+// $('html').html('asd');
+$(".main_nav").click(function(){
+    $(this).hide()
   });
 
 
+// $(document).ready(function(){
+//   $(".main_screen__sidebar").click(function(){
+//     $(this).css("background-color","#70a9db")
+//   });
+//    });
 
 
-});
+
+// $(document).ready(function(){
+//  $(".main_screen").click(function(){
+//  $(this).hide()
+//  }
+
+// };
+
+
+// $(document).ready(function(){
+//   // $("#navigation").click();
+
+//     $(".main_screen__title").click(function(){
+//     $(this).css("background-color","red")
+//   });
+
+
+// $(window).resize(function(){
+//             var w = $(window).width();
+//             if(w > 320 && '.main_nav'.is(':hide')) {
+//               $'.main_nav'.hide();
+//             }
+// $(window).resize(function() {
+//   if(document > 320) {
+//     $(".main_nav").hide(1500);
+//     // тут ваш скрипт
+//   }
+
