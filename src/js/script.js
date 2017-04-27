519331b40dca35e0da94bbf98a214b3a3791b662
KarryNefedieva <karinefedieva@gmail.com>
2017-04-27 18:22:40 +0300

    мой первый jquery

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,19 @@
-// $('html').html('asd');
-$(".main_nav").click(function(){
-    $(this).hide()
+
+
+$(document).ready(function(){
+
+  $('.main_screen__gamb').click(function(){
+    $(".main_block").addClass("open");
+    $('.main_block').removeClass('close');
+  });
+
+   $('.main_nav__close').click(function(){
+    $('.main_block').addClass('close');
+    $('.main_block').removeClass('open');
+
+  });
+
   });
 
 
-// $(document).ready(function(){
-//   $(".main_screen__sidebar").click(function(){
-//     $(this).css("background-color","#70a9db")
-//   });
-//    });
-
-
-
-// $(document).ready(function(){
-//  $(".main_screen").click(function(){
-//  $(this).hide()
-//  }
-
-// };
-
-
-// $(document).ready(function(){
-//   // $("#navigation").click();
-
-//     $(".main_screen__title").click(function(){
-//     $(this).css("background-color","red")
-//   });
-
-
-// $(window).resize(function(){
-//             var w = $(window).width();
-//             if(w > 320 && '.main_nav'.is(':hide')) {
-//               $'.main_nav'.hide();
-//             }
-// $(window).resize(function() {
-//   if(document > 320) {
-//     $(".main_nav").hide(1500);
-//     // тут ваш скрипт
-//   }
 
