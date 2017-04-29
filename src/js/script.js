4e5bd482cea7d76163d06979c6ea2abed5c69da9
KarryNefedieva <karinefedieva@gmail.com>
2017-04-29 22:54:51 +0300

    конец верстке 1.10

--- src/js/script.js
+++ src/js/script.js
@@ -10,10 +10,24 @@ $(document).ready(function(){
    $('.main_nav__close').click(function(){
     $('.main_block').addClass('close');
     $('.main_block').removeClass('open');
+ });
+
+    $('.contacts__filial_name_fin').click(function(){
+    $('#modal_finland').addClass('modal__filial');
+ });
+
+
+
+
 
   });
 
-  });
+// $(document).ready(function(){
+//   $('.your-class').slick({
+//     setting-name: setting-value
+//   });
+// });
+
 
 
 
